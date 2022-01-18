5ac254f8070df4c8892784c9a5ac48eb2a3f3249
Hydrataur <amiteinny@gmail.com>
2022-01-18 18:26:47 +0200

    Added overrides for the rest of the default cli

--- src/storybook-generator/index.ts
+++ src/storybook-generator/index.ts
@@ -16,3 +16,87 @@ export function storybookGenerator(_options: any): Rule {
 		]);
 	};
 }
+
+export function createAppShell(_options: any): Rule {
+	return (_tree: Tree, _context: SchematicContext) => {
+		return externalSchematic('@schematics/angular', 'app-shell', _options);
+	}
+}
+
+export function createApplication(_options: any): Rule {
+	return (_tree: Tree, _context: SchematicContext) => {
+		return externalSchematic('@schematics/angular', 'application', _options);
+	}
+}
+
+export function createClass(_options: any): Rule {
+	return (_tree: Tree, _context: SchematicContext) => {
+		return externalSchematic('@schematics/angular', 'class', _options);
+	}
+}
+
+export function createDirective(_options: any): Rule {
+	return (_tree: Tree, _context: SchematicContext) => {
+		return externalSchematic('@schematics/angular', 'directive', _options);
+	}
+}
+
+export function createEnum(_options: any): Rule {
+	return (_tree: Tree, _context: SchematicContext) => {
+		return externalSchematic('@schematics/angular', 'enum', _options);
+	}
+}
+
+export function createGuard(_options: any): Rule {
+	return (_tree: Tree, _context: SchematicContext) => {
+		return externalSchematic('@schematics/angular', 'guard', _options);
+	}
+}
+
+export function createInterface(_options: any): Rule {
+	return (_tree: Tree, _context: SchematicContext) => {
+		return externalSchematic('@schematics/angular', 'interface', _options);
+	}
+}
+
+export function createLibrary(_options: any): Rule {
+	return (_tree: Tree, _context: SchematicContext) => {
+		return externalSchematic('@schematics/angular', 'library', _options);
+	}
+}
+
+export function createModule(_options: any): Rule {
+	return (_tree: Tree, _context: SchematicContext) => {
+		return externalSchematic('@schematics/angular', 'module', _options);
+	}
+}
+
+export function createPipe(_options: any): Rule {
+	return (_tree: Tree, _context: SchematicContext) => {
+		return externalSchematic('@schematics/angular', 'pipe', _options);
+	}
+}
+
+export function createResolver(_options: any): Rule {
+	return (_tree: Tree, _context: SchematicContext) => {
+		return externalSchematic('@schematics/angular', 'resolver', _options);
+	}
+}
+
+export function createService(_options: any): Rule {
+	return (_tree: Tree, _context: SchematicContext) => {
+		return externalSchematic('@schematics/angular', 'service', _options);
+	}
+}
+
+export function createServiceWorker(_options: any): Rule {
+	return (_tree: Tree, _context: SchematicContext) => {
+		return externalSchematic('@schematics/angular', 'service-worker', _options);
+	}
+}
+
+export function createWebWorker(_options: any): Rule {
+	return (_tree: Tree, _context: SchematicContext) => {
+		return externalSchematic('@schematics/angular', 'web-worker', _options);
+	}
+}
